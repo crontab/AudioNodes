1f0ee32371fc77a6b36b70974d0f73e930427e16
Hovik Melikyan <hovik.melikyan@gmail.com>
2024-08-24 13:17:30 +0400

    SineGenerator as a static source

--- AudioNodes/Sources/SineGenerator.swift
+++ AudioNodes/Sources/SineGenerator.swift
@@ -8,11 +8,15 @@
 import Foundation
 
 
-final class SineGenerator: Source {
+final class SineGenerator: Source, StaticDataSource {
+
+	let format: StreamFormat
+
 
 	init(freq: Float32, format: StreamFormat, isEnabled: Bool = false) {
-		freq$ = freq
-		thetaInc = 2.0 * Double.pi / format.sampleRate
+		self.format = format
+		self.freq$ = freq
+		thetaInc = 2.0 * .pi / format.sampleRate
 		precondition(thetaInc > 0)
 		super.init(isEnabled: isEnabled)
 	}
@@ -34,10 +38,10 @@ final class SineGenerator: Source {
 		let samples = buffers[0].samples
 		let increment = thetaInc * Double(_freq)
 		for frame in 0..<frameCount {
-			samples[frame] = Sample(sin(_theta) * Self.amplitude)
+			samples[frame] = Sample(sin(_theta))
 			_theta += increment
-			if _theta > 2.0 * Double.pi {
-				_theta -= 2.0 * Double.pi
+			if _theta > 2.0 * .pi {
+				_theta -= 2.0 * .pi
 			}
 		}
 		for i in 1..<buffers.count {
@@ -47,10 +51,16 @@ final class SineGenerator: Source {
 	}
 
 
+	// Static source protocol
+	func readSync(frameCount: Int, buffers: AudioBufferListPtr, numRead: inout Int) -> OSStatus {
+		numRead = frameCount
+		return _render(frameCount: frameCount, buffers: buffers)
+	}
+
+
 	private let thetaInc: Double
 
 	private var freq$: Float
 	private var _freq: Float = 0
 	private var _theta: Double = 0
-	private static let amplitude = 1.0
 }
