f41379dcebdd2f68d005750b37da2297af7aa7bb
Hovik Melikyan <hovik.melikyan@gmail.com>
2025-01-25 16:45:26 +0100

    AudioNodes framework (I)

---
+++ AudioNodes/Sources/AudioNodes.h
@@ -0,0 +1,18 @@
+//
+//  AudioNodes.h
+//  AudioNodes
+//
+//  Created by Hovik Melikyan on 25.01.25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AudioNodes.
+FOUNDATION_EXPORT double AudioNodesVersionNumber;
+
+//! Project version string for AudioNodes.
+FOUNDATION_EXPORT const unsigned char AudioNodesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AudioNodes/PublicHeader.h>
+
+
